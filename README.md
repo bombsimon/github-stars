55f54c413b2662dee6be3bb03ac0a064ab9e5bc8
Simon Sawert <simon@sawert.se>
2020-08-06 20:06:13 +0200

    Make the app (somewhat) useful

--- README.md
+++ README.md
@@ -1,4 +1,53 @@
 # GitHub Stars
 
-This is a tool to fetch the total number of stars for all repositories from a
-user.
+Fetch repositories and how many ⭐️ they have. Useful if you don't get any likes
+on Twitter or Instagram but can write good code. Will probably help if your
+coding technique is hype driven so it always uses The Latest and Greatest™
+software!
+
+## Usage
+
+Show help for flags and arguments.
+
+```sh
+$ github-stars --help
+GitHub Stars 0.1
+Simon Sawert <simon@sawert.se>
+Get stars from GitHub user repositories
+
+USAGE:
+    github-stars [OPTIONS] <username>
+
+ARGS:
+    <username>    GitHub username
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+OPTIONS:
+    -t, --threshold <threshold>    Minimum stars to show [default: 1]
+```
+
+Fetch user stars. Set threshold to 0 to fetch all repositories no matter if
+they're starred or not.
+
+```sh
+$ github-stars sharkdp --threshold 100
+⭐️ 21939 | bat              | A cat(1) clone with wings.
+⭐️ 14500 | fd               | A simple, fast and user-friendly alternative to 'find'
+⭐️  5833 | hyperfine        | A command-line benchmarking tool
+⭐️  5467 | hexyl            | A command-line hex viewer
+⭐️  2910 | pastel           | A command-line tool to generate, analyze, convert and manipulate colors
+⭐️  2122 | insect           | High precision scientific calculator with support for physical units
+⭐️  1440 | cube-composer    | A puzzle game inspired by functional programming
+⭐️  1330 | dbg-macro        | A dbg(…) macro for C++
+⭐️   908 | shell-functools  | Functional programming tools for the shell
+⭐️   606 | diskus           | A minimal, fast alternative to 'du -sh'
+⭐️   426 | great-puzzles    | A curated list of great puzzles
+⭐️   358 | vivid            | A generator for LS_COLORS with support for multiple color themes
+⭐️   263 | purescript-flare | A special-purpose UI library for Purescript
+⭐️   154 | trigger          | Run a user-defined command on file changes
+
+ Total stars: 59254
+```
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use reqwest::Error;
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
@@ -15,9 +14,72 @@ struct Repository {
     stargazers_count: u32,
 }
 
+struct RepositoryResult {
+    repositories: Vec<Repository>,
+    total_stars: usize,
+    longest_repo_name: usize,
+    longest_start_count: usize,
+}
+
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    // Name your user agent after your app?
+async fn main() -> Result<(), &'static str> {
+    let matches = clap::App::new("GitHub Stars")
+        .version("0.1")
+        .author("Simon Sawert <simon@sawert.se>")
+        .about("Get stars from GitHub user repositories")
+        .arg(
+            clap::Arg::with_name("username")
+                .about("GitHub username")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            clap::Arg::with_name("threshold")
+                .short('t')
+                .long("threshold")
+                .about("Minimum stars to show")
+                .takes_value(true)
+                .required(false)
+                .default_value("1"),
+        )
+        .get_matches();
+
+    let username = matches.value_of("username").unwrap();
+    let star_threashold = matches
+        .value_of("threshold")
+        .unwrap_or("1")
+        .parse::<u32>()
+        .unwrap_or(1);
+
+    let result = match get_user_repos(username.to_string(), star_threashold).await {
+        Ok(v) => v,
+        Err(e) => {
+            println!("error fetching stars: {}", e);
+
+            return Err("could not count stars");
+        }
+    };
+
+    for repo in result.repositories {
+        println!(
+            "⭐️ {:star_width$} | {:name_width$} | {}",
+            repo.stargazers_count,
+            repo.name,
+            repo.description.unwrap_or(String::from("-")),
+            name_width = result.longest_repo_name,
+            star_width = result.longest_start_count,
+        );
+    }
+
+    println!("\n Total stars: {}", result.total_stars);
+
+    Ok(())
+}
+
+async fn get_user_repos(
+    username: String,
+    star_threashold: u32,
+) -> Result<RepositoryResult, reqwest::Error> {
     static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
     let client = reqwest::Client::builder()
@@ -25,19 +87,23 @@ async fn main() -> Result<(), Error> {
         .build()?;
 
     let page_limit = 100u32;
-    let user_request_url = format!("https://api.github.com/users/{owner}", owner = "bombsimon");
+    let user_request_url = format!("https://api.github.com/users/{owner}", owner = username);
     let user_result = client.get(&user_request_url).send().await?;
+
+    user_result.error_for_status_ref()?;
+
     let user: User = user_result.json().await?;
     let total_pages = (user.public_repos as f32 / page_limit as f32).ceil() as i32;
 
-    let mut all_repos: Vec<Repository> = Vec::new();
+    let mut repositories: Vec<Repository> = Vec::new();
+    let mut total_stars = 0usize;
     let mut longest_repo_name = 0;
     let mut longest_start_count = 0;
 
     for x in (0..total_pages).rev() {
         let repo_request_url = format!(
             "https://api.github.com/users/{owner}/repos?per_page={page_limit}&page={page}",
-            owner = "bombsimon",
+            owner = username,
             page_limit = page_limit,
             page = (x + 1)
         );
@@ -46,6 +112,12 @@ async fn main() -> Result<(), Error> {
         let repos: Vec<Repository> = repo_result.json().await?;
 
         for r in repos {
+            total_stars += r.stargazers_count as usize;
+
+            if r.stargazers_count < star_threashold {
+                continue;
+            }
+
             let repo_name_length = r.name.len();
             let repo_star_count_as_str_length = r.stargazers_count.to_string().len();
 
@@ -57,26 +129,16 @@ async fn main() -> Result<(), Error> {
                 longest_start_count = repo_star_count_as_str_length;
             }
 
-            all_repos.push(r)
+            repositories.push(r)
         }
     }
 
-    all_repos.sort_by(|a, b| b.stargazers_count.cmp(&a.stargazers_count));
+    repositories.sort_by(|a, b| b.stargazers_count.cmp(&a.stargazers_count));
 
-    for repo in all_repos {
-        if repo.stargazers_count < 1 {
-            break;
-        }
-
-        println!(
-            "{:name_width$} ⭐️ {:star_width$} {}",
-            repo.name,
-            repo.stargazers_count,
-            repo.description.unwrap_or(String::from("-")),
-            name_width = longest_repo_name,
-            star_width = longest_start_count,
-        );
-    }
-
-    Ok(())
+    Ok(RepositoryResult {
+        repositories,
+        total_stars,
+        longest_repo_name,
+        longest_start_count,
+    })
 }
